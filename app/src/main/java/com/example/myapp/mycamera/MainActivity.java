358c7a559724950abd7ed97f513955b17dc816d1
markbrobyn <markbrobyn@yahoo.co.uk>
2015-09-25 15:45:43 +0100

    Taking thumbnail image

--- app/src/main/java/com/example/myapp/mycamera/MainActivity.java
+++ app/src/main/java/com/example/myapp/mycamera/MainActivity.java
@@ -1,17 +1,55 @@
 package com.example.myapp.mycamera;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+// notes
+//   http://developer.android.com/training/camera/photobasics.html
+//     to test for camera if AndroidManifest uses-feature android.hardware.camera required="false"
+//       hasSystemFeature(PackageManager.FEATURE_CAMERA)
+// can't connect to camera error
+//   Genymotion: Switch off camera
+//   Intellij IDEA: From menu select Tools/Android/"Enable ADB Integration" (should be unchecked)
+//   Genymotion: Switch on camera
 
 
 public class MainActivity extends ActionBarActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView myImageView;
+
+    public void takePhoto(View view){
+        dispatchTakePictureIntent();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            myImageView =(ImageView)findViewById(R.id.myImageView);
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            myImageView.setImageBitmap(imageBitmap);
+        }
     }
 
     @Override
